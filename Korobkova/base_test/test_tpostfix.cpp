4b00425444a6d4e7fb5c4a9d54e63863d8c88f3f
Екатерина <korobkova.ek99@mail.ru>
2019-11-03 19:32:50 +0300

    Task 3

--- Korobkova/base_test/test_tpostfix.cpp
+++ Korobkova/base_test/test_tpostfix.cpp
@@ -6,5 +6,33 @@ TEST(TPostfix, can_create_postfix)
   ASSERT_NO_THROW(TPostfix p);
 }
 
+TEST(TPostfix, convert_infix_to_postfix)		//преобразование
+{
+	TPostfix v("a+b*c");
+	v.ToPostfix();
+	EXPECT_EQ(p.GetPostfix(), "bc*a+");
+}
 
+TEST(TPostfix, convert_equal_expression)			//равенство одинаковых выражений
+{
+	TPostfix v("(a+b*c)");
+	v.ToPostfix();
+	TPostfix n("a+b*c");
+	n.ToPostfix();
+	EXPECT_EQ(v.GetPostfix(), n.GetPostfix());
+}
+
+TEST(TPostfix, calculation_accuary)					//корректность вычисления
+{
+	TPostfix v("2+2*2");
+	v.ToPostfix();
+	EXPECT_EQ(p.Calculate(), 6);
+}
+
+TEST(TPostfix, convert_infix_with_brackets)		//правильная обработка выражений со скобками
+{
+	TPostfix v("(a+b)*c+d");
+	v.ToPostfix();
+	EXPECT_EQ(v.GetPostfix(), "ab+c*d+");
+}
 
